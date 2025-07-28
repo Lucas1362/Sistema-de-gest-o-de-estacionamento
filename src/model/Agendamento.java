fdda08745bc59cb8e8b19eead3260435b57da0a4
ucas tenorio <jamersontenorio88@gmail.com>
2025-07-28 19:26:44 -0300

    classes estacionamento e agendamento levemente polidas

--- src/model/Agendamento.java
+++ src/model/Agendamento.java
@@ -6,4 +6,27 @@ public class Agendamento{
     private Veiculo veiculo;
     private Vaga vaga;
     private localDateTime horario;
+
+    public Agendamento(Veiculo veiculo, Vaga vaga, LocalDateTime horario){
+        this.veiculo = veiculo;
+        this.vaga = vaga;
+        this.horario = horario;
+
+    }
+
+    //getters
+
+
+    public Veiculo getVeiculo() {
+        return veiculo;
+    }
+
+    public Vaga getVaga() {
+        return vaga;
+    }
+
+    public localDateTime getHorario() {
+        return horario;
+    }
+
 }
--- src/model/Estacionamento.java
+++ src/model/Estacionamento.java
@@ -1,13 +1,55 @@
-package model
+package src.model;
+
+import src.model.Vaga;
+import src.model.Veiculo;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.list;
+
 
 public class Estacionamento {
     private List<Vaga> vagas;
-    private List<Agendamento> agendamento;
+    private List<Veiculo> veiculos;
 
+
+//construtores
+
+    public Estacionamento(){
+        this.vagas = new ArrayList<>();
+        this.veiculos = new ArrayList<>();
+
+        // quantidade definida de vagas
+        for (int i = 1; i <= 10; i++){
+            vagas.add(new Vaga(i));
+        }
+    }
 
     public boolean reservarVaga(Veiculo veiculo, LocalDateTime horario) {
-        return true
+        for (Vaga vaga : vagas){
+            if(!vaga.isOcupada()){
+                vaga.setOcupada(true);
+                veiculos.add(veiculo);
+                return true;// em caso de reserva com sucesso
+            }
+        }
+        return false; // vagas indisponiveis
 
+
+    }
+    //registro de entrada
+    public void registrarEntrada(Veiculo veiculo){
+
+    }
+
+    // obter informações sobre as vagas disponiveis
+    public List<Vaga> getVagasDisponiveis(){
+        List<Vaga> vagasDisponiveis = new ArrayList<>();
+        for(Vaga vaga : vagas){
+            if (!vaga.isOcupada()){
+                vagasDisponiveis.add(vaga);
+            }
+        }
+        return vagasDisponiveis;
     }
 
     public void registrarEntrada(Veiculo veiculo) {
