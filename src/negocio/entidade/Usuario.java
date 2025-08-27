f024a9f6570e91c81beea0ce36e74b080e89b8eb
ucas tenorio <jamersontenorio88@gmail.com>
2025-08-27 20:02:08 -0300

    gerente com esqueleto mais definido

--- src/negocio/entidade/Usuario.java
+++ src/negocio/entidade/Usuario.java
@@ -1,26 +1,33 @@
 package negocio.entidade;
 
-public class Usuario{
+public abstract class Usuario {
     private String nome;
     private String cpf;
 
-    public Cliente(String nome, String cpf){
+    public Usuario(String nome, String cpf) {
         this.nome = nome;
         this.cpf = cpf;
     }
 
-    public String getNome(){
+    // Getters e Setters
+    public String getNome() {
         return nome;
-    }
-    public String getCpf(){
-        return cpf;
     }
 
     public void setNome(String nome) {
         this.nome = nome;
     }
 
+    public String getCpf() {
+        return cpf;
+    }
+
     public void setCpf(String cpf) {
         this.cpf = cpf;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s - CPF: %s", nome, cpf);
     }
 }
