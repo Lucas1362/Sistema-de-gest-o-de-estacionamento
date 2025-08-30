dc2b4e17e1074800a1f9ca9c5717e7d31b2f6ae8
Jamerson Lucas Tenorio Valentim <jamersontenorio88@gmail.com>
2025-08-30 19:58:29 -0300

    update gerente

--- src/negocio/entidade/Veiculo.java
+++ src/negocio/entidade/Veiculo.java
@@ -1,38 +1,40 @@
 package negocio.entidade;
 import java.io.Serializable;
 
-public class Veiculo implements Serializable{
+
+public class Veiculo implements Serializable {
+
     private static final long serialVersionUID = 1L;
     private String placa;
-    private Cliente dono;
+    private ClienteUsuario dono; // Alterado de Cliente para ClienteUsuario
 
-    public Veiculo(String placa, Cliente dono){
+    /**
+     * Construtor para a classe Veiculo.
+     * @param placa A placa do veículo.
+     * @param dono  O ClienteUsuario que é o dono do veículo.
+     */
+    public Veiculo(String placa, ClienteUsuario dono) { // Alterado o tipo do parâmetro
         this.placa = placa;
         this.dono = dono;
     }
 
-    // getters classe Veiculo
-
+    //Getters
 
     public String getPlaca() {
         return placa;
-
     }
 
-    public Cliente getDono(){
+    public ClienteUsuario getDono() { // Alterado o tipo de retorno
         return dono;
-
     }
 
-    //setters
-    public void setPlaca(String placa){
+    //Setters
+
+    public void setPlaca(String placa) {
         this.placa = placa;
-
     }
 
-    public void setDono(Cliente dono){
+    public void setDono(ClienteUsuario dono) { // Alterado o tipo do parâmetro
         this.dono = dono;
     }
-
-
 }
